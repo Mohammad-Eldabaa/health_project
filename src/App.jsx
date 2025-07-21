8dd1ddfa2934c41073be64f041b125b484730f21
ahmed-elbedewy <ahmedelbedewy21@gmail.com>
2025-07-21 21:43:39 +0300

    home page has updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Layout from './Components/Layout/Layout'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import Services from './Components/Services/Services'
+import Notfound from './Components/Notfound/Notfound'
+import Booking from './Components/Booking/Booking' 
+import Footer from './Components/Footer/Footer'
+
+const router = createBrowserRouter([
+  {
+    path: '', element: <Layout />, children: [
+      {
+        index: true, element: <Home />
+      },
+      {
+        path: 'about', element: <About />
+      },
+      {
+        path: 'contact', element: <Contact />
+      },
+      {
+        path: 'services', element: <Services />
+      },
+      {
+        path: 'booking', element: <Booking />
+      },
+      {
+        path: 'footer', element: <Footer />
+      },
+      {
+        path: '*', element: <Notfound />
+      }
+    ]
+  }
+])
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Style from './About.module.css'
+export default function About() {
+  return (
+    <>
+      <h1>about</h1>
+    </>
+  )
+}
