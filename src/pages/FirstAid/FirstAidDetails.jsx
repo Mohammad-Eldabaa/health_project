e3c24291bd34e34b93efcdddbc3f6d012693229a
Hamada Abo Alatta <hamadawael1011@gmail.com>
2025-08-03 05:41:11 +0300

    Amendments and improvements to my first aid and medical articles pages.

--- src/pages/FirstAid/FirstAidDetails.jsx
+++ src/pages/FirstAid/FirstAidDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import useFirstAidStore from '../../store/firstaid';
 import styles from './FADetails.module.css';
+import Navbar from '../../components/Navbar/Navbar';
 
 export default function FirstAidDetails() {
   const [htmlContent, setHtmlContent] = useState('');
@@ -21,15 +22,14 @@ export default function FirstAidDetails() {
 
   return (
     <>
+            <Navbar />
       <div className="flex flex-col min-h-screen bg-gray-100">
-        <div className="w-full bg-gray-300 min-h-[50vh]bg- flex items-center justify-center px-4">
+        <div className="w-full bg-gray-300 min-h-[50vh] flex items-center justify-center px-4 pt-15">
           <div className="w-full sm:w-[90%] md:w-[85%] lg:w-[85%] aspect-video rounded-xl overflow-hidden shadow-lg my-4">
             <iframe
               className="w-full h-full"
               src={state.video}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
+              title="First Aid Video"
               allowFullScreen
             ></iframe>
           </div>
